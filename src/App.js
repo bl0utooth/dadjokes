d26b52fbdc0f0e1ae58075b7cf22a91e1b7bb354
bl0utooth <136910180+bl0utooth@users.noreply.github.com>
2024-05-28 17:06:11 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import JokeList from './JokeList';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <JokeList />
+    </div>
+  )
+}
+
+export default App;
